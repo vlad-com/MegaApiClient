d7ef3b06d4c34167cf415e875c15d0c605b7e602
Vlad <139916712+vlad-com@users.noreply.github.com>
2023-10-17 13:51:32 +0000

    aes-ni support

--- MegaApiClient/Cryptography/Crypto.cs
+++ MegaApiClient/Cryptography/Crypto.cs
@@ -19,7 +19,8 @@
       s_aesCbc = Aes.Create(); // More per-call overhead but supported everywhere.
       s_isKnownReusable = false;
 #else
-      s_aesCbc = new AesManaged();
+      s_aesCbc = new AesCryptoServiceProvider(); // aes-ni support
+      // s_aesCbc = new AesManaged();
       s_isKnownReusable = true;
 #endif
 
